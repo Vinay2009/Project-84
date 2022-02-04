9ab3aeef853337b56463f384d8ccdc114a882538
Vinay2009 <91785104+Vinay2009@users.noreply.github.com>
2022-02-04 17:51:51 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,100 +1,58 @@
-canvas = document.getElementById('myCanvas');
-ctx = canvas.getContext("2d");
+// Create a reference for the canvas
 
-nasa_mars_images_array = ["nasa_image_1.jpg", "nasa_image_2.jpeg", "nasa_image_3.jpg", "nasa_image_4.jpg"];
+img_width = 300;
+img_height = 100;
 
-random_number = Math.floor(Math.random() * 4);
-console.log(random_number);
+var img_image;
 
-
-background_image = nasa_mars_images_array[random_number];
-console.log("background_image = "+ background_image);
-
-rover_image = "rover.jpg";
-rover_width = 100;
-rover_height = 90;
-
-rover_x = 10;
-rover_y = 10;
+img_x = 100;
+img_y = 100;
 
 function add() {
-    background_imgTag = new Image(); //defining a variable with a new image
-    background_imgTag.onload = uploadBackground; // setting a function, onloading this variable
-    background_imgTag.src = background_image; // load image
+	img_imgTag = new Image(); //defining a variable with a new image
+	img_imgTag.onload = uploadimg; // setting a function, onloading this variable
+	img_imgTag.src = img_image;   // load image
+}
 
-    rover_imgTag = new Image();//defining a variable with a new image
-    rover_imgTag.onload = uploadrover; //setting a function, onloading this variable
-    rover_imgTag.src = rover_image; //load image
+function uploadimg() {
+
+	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
 }
-function uploadBackground() {
-    ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
-}
-function uploadrover() {
-    ctx.drawImage(rover_imgTag, rover_x, rover_y, rover_width, rover_height);
-}
-window.addEventListener("keydown", my_keydown);
+
+//Write a code to grab the key-pressed event
 
 function my_keydown(e)
 {
-    keyPressed = e.keyCode;
-    console.log(keyPressed);
-    if(keyPressed == '38')
-    {
-        up();
-        console.log("up");
-    }
-    if(keyPressed == '40')
-    {
-        down();
-        console.log("down");
-    }
-    if(keyPressed == '37')
-    {
-        left();
-        console.log("left");
-    }
-    if(keyPressed == '39')
-    {
-        right();
-        console.log("right");
-    }
-    }
-    function up()
-    {
-        if(rover_y >=0)
-        {
-            rover_y = rover_y - 10;
-            console.log("When up arrow is pressed, x=" + rover_x + ", y =" +rover_y);
-            uploadBackground();
-            uploadrover();
-        }
-    }
-    function down()
-    {
-        if(rover_y <=500)
-        {
-            rover_y = rover_y + 10;
-            console.log("When down arrow is pressed, x=" + rover_x + ", y =" +rover_y);
-            uploadBackground();
-            uploadrover();
-        }
-    }
-    function left()
-    {
-        if(rover_x >=0)
-        {
-            rover_x = rover_x - 10;
-            console.log("When left arrow is pressed, x=" + rover_x + ", y =" +rover_y);
-            uploadBackground();
-            uploadrover();
-        }
-    }
-    function right(){
-        if(rover_x <=700)
-        {
-            rover_x = rover_x + 10;
-            console.log("When right arrow is pressed, x=" + rover_x + ", y =" +rover_y);
-            uploadBackground();
-            uploadrover();
-        }
-    }
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	
+		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
+		//write a code to check the type of key pressed
+	else{
+		otherkey();
+		document.getElementById("d1").innerHTML="You pressed symbol or other key";
+	}
+}
+
+function aplhabetkey()
+{
+	//upload respective image with the message. 
+
+}
+function numberkey()
+{
+	
+}
+function arrowkey()
+{
+}
+function specialkey()
+{
+	
+}
+function otherkey()
+{
+	img_image="otherkey.png";
+	add();
+}
+	
