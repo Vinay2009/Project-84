3c48cf1e79f1e71bf4cfbb568ecf1d5de55caea2
Vinay2009 <91785104+Vinay2009@users.noreply.github.com>
2022-03-25 16:50:30 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -33,6 +33,21 @@ function my_keydown(e)
 			document.getElementById("d1").innerHTML="You pressed Aplhabet Key";
 			console.log("alphabet key");
 		}
+		else if(keyPressed >=48 && keyPressed<=57){
+         numberkey();
+		 document.getElementById("d1").innerHTML="You pressed Number Key";
+			console.log("number key");
+		}
+		else if(keyPressed >=37 && keyPressed<=40){
+			arrowkey();
+		 document.getElementById("d1").innerHTML="You pressed Arrow Key";
+			console.log("arrow key");
+		}
+		else if(keyPressed==17 || keyPressed==18 || keyPressed==27){
+			specialkey();
+			document.getElementById("d1").innerHTML="You pressed Special Key";
+			console.log("special key");
+		}		
 	    else{
 		otherkey()
 		document.getElementById("d1").innerHTML="You pressed symbol or other key"};
